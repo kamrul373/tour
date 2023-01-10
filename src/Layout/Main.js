a7af16f57649cb816c4d2ed3beb962e8dce571e4
unknown <textile.zero@gmail.com>
2023-01-11 02:39:12 +0600

    banner area added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import { FaSearch } from "react-icons/fa";
 import Nav from '../components/Shared/Nav/Nav';
-import { Outlet } from 'react-router-dom';
 
 const Main = () => {
 	return (
 		<div>
-			<Nav></Nav>
-			<Outlet></Outlet>
+			<div className="drawer">
+				<input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+				<div className="drawer-content flex flex-col">
+					{/* <!-- Navbar --> */ }
+					<div className="w-full navbar px-20 py-5 fixed top-0 right-0 left-0 z-50 text-white">
+						<div className="flex-none lg:hidden">
+							<label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+							</label>
+						</div>
+						<div className="flex-1 px-2 mx-2 font-bold">
+							<Link to="/">Agumentik</Link>
+						</div>
+						<div className="flex-none hidden lg:block ">
+							<Nav></Nav>
+						</div>
+					</div>
+					<Outlet></Outlet>
+				</div>
+				<div className="drawer-side">
+					<label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+					<ul className="menu p-4 w-80 bg-base-100">
+						{/* <!-- Sidebar content here --> */ }
+						<li><Link to="/">Home</Link></li>
+						<li><Link to="/">Discover</Link></li>
+						<li><Link to="/">History</Link></li>
+						<li><Link to="/">Events</Link></li>
+						<li><Link to="/">Blogs</Link></li>
+						<li><Link to="/">Interests</Link></li>
+						<li>
+							<span>Search <FaSearch className='text-xl font-semibold text-black'></FaSearch></span>
+
+						</li>
+
+					</ul>
+
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import bg from "../../assets/banner.png";
+import "./banner.css";
 const Banner = () => {
 	return (
-		<div>
+		<div className='min-h-screen banner' style={ { backgroundImage: `url(${ bg })` } }>
+			<div className="grid lg:grid-cols-2 grid-cols-1 align-items-center px-20">
+				<div className="banner-left">
+					<div className="content-area py-52">
+						<h2 className='lg:text-8xl text-3xl text-white font-bold'>
+							Welcome to Srilanka
+						</h2>
+					</div>
+				</div>
+				<div className="banner-right">
+					<div className="slider-area">
 
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
